5ff1fc2182aac3593a45a78b4d9053172ff37f04
a9sk <920a9sk42f76c765@proton.me>
2024-06-13 22:37:35 +0200

    added modified format version

--- steno.sh
+++ steno.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 
 usage(){
-    clear
     echo "Usage: stenorun <filename> -fF <flag format>"
     echo ""
     echo "Flag Format"
@@ -16,22 +15,48 @@ usage(){
 }
 
 file_check(){
-    if [ ! -e FILENAME ] ; then
-        echo "[!] The file does not exist"
+    if [ ! -f $FILEPATH ] ; then
+        echo "[!] The file does not exist"; echo ""
         sleep 0.1
-        echo "[*] Exiting..."
-        exit
+        usage
     fi
     echo "[*] The file exists, starting the various tools..."
     sleep 0.2
 }
 
-main(){
-    file_check
+set_format(){
+    if [ "$1" == '-fF' ]; then # 1 is 2 and 2 is 3...
+        FORMAT="$2"
+        echo "[*] The flag format is set to $FORMAT{flag_goes_here}"
+    else
+        echo "[!] Invalid parameters passed"; echo ""
+        sleep 0.1
+        usage
+    fi
 }
+
+clear
 
 declare -g FORMAT
 declare -g FILENAME
-FILENAME=$1
+declare -g CURRENTPATH
+declare -g FILEPATH
 
-main
+CURRENTPATH="$(pwd)"
+
+if [ -z "$1" ]; then
+    echo "[!] No parameter was passed..."; echo ""
+    sleep 0.1
+    usage
+fi
+
+FILENAME=$1
+FILEPATH="$CURRENTPATH/$FILENAME"
+
+file_check
+if [ -z "$2" ] || [ -z "$3" ]; then #if parameter 3 exist then parameter 2 also exists
+    FORMAT="flag"
+    echo "[*] The flag format is set to $FORMAT{flag_goes_here}"
+else
+    set_format $2 $3
+fi
