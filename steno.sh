b906ffa950dfb662dbad79d11785c3c69488eb7a
a9sk <920a9sk42f76c765@proton.me>
2024-06-13 20:39:41 +0200

    added check to see if the file exists

--- steno.sh
+++ steno.sh
@@ -14,3 +14,24 @@ usage(){
     sleep 1
     exit
 }
+
+file_check(){
+    if [ ! -e FILENAME ] ; then
+        echo "[!] The file does not exist"
+        sleep 0.1
+        echo "[*] Exiting..."
+        exit
+    fi
+    echo "[*] The file exists, starting the various tools..."
+    sleep 0.2
+}
+
+main(){
+    file_check
+}
+
+declare -g FORMAT
+declare -g FILENAME
+FILENAME=$1
+
+main
