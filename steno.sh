9068c95d8ca4ffb3952d1ae1fe73d7f6f4d5e2c4
a9sk <920a9sk42f76c765@proton.me>
2024-06-13 20:12:41 +0200

    added initial usage message

---
+++ steno.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+usage(){
+    clear
+    echo "Usage: stenorun <filename> -fF <flag format>"
+    echo ""
+    echo "Flag Format"
+    echo "      -fF <flag format> to set the flag format (i.e. if -fF is set to 'abc' the flag format will be abc{flag_goes_here})"
+    echo ""
+    echo "Miscellaneous"
+    echo "      -h  show this message and exits"
+    echo ""
+    echo "@a9sk"
+    sleep 1
+    exit
+}
