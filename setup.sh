3e65db32b402318c47a8512482af52469f816b46
a9sk <167626445+a9sk@users.noreply.github.com>
2024-06-14 11:22:42 +0200

    added strings installation

--- setup.sh
+++ setup.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+if ! command -v strings &> /dev/null
+then
+    read -p "$(echo -e '[?] Strings not installed, do you want to install it?'$i' (y/n) ')" ok
+    if [[ $ok == "" ]]
+    then
+        sudo apt-get update
+        sudo apt-get install strings
+    else
+        case $ok in
+            [Yy]* )
+                echo "[*] Installing strings..."
+                sudo apt-get update
+                sudo apt-get install strings
+                if command -v strings &> /dev/null; then
+                    echo "[*] Ok, strings installed correctly"
+                else
+                    echo "[!] There was a problem installing strings, try manual installation"
+                fi;;
+            * )  echo "[*] Ok, strings will not be installed";;
+        esac
+    fi
+else
+    echo "[*] Strings is installed, continuing..."
+fi
