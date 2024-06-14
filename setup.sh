a528988139358ad3edbd439f064426fdf4b14f6a
a9sk <920a9sk42f76c765@proton.me>
2024-06-14 21:49:51 +0200

    added exiv2 installation

--- setup.sh
+++ setup.sh
@@ -48,3 +48,26 @@ then
 else
     echo "[*] Exiftool is installed, continuing..."
 fi
+
+if ! command -v exiv2 &> /dev/null
+then
+    read -p "$(echo -e '[?] Exiv2 not installed, do you want to install it?'$i' (y/n) ')" ok
+    if [[ $ok == "" ]]
+    then
+        sudo apt-get -y install exiv2
+    else
+        case $ok in
+            [Yy]* )
+                echo "[*] Installing exiv2..."
+                sudo apt-get -y install exiv2
+                if command -v exiv2 &> /dev/null; then
+                    echo "[*] Ok, exiv2 installed correctly"
+                else
+                    echo "[!] There was a problem installing exiv2, try manual installation"
+            fi;;
+            * )  echo "[*] Ok, exiv2 will not be installed";;
+        esac
+    fi
+else
+    echo "[*] Exiv2 is installed, continuing..."
+fi
